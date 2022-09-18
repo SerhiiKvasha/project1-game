d2a174bdc27b09ad8d59dd9ed8ff368073bf144b
Serhii Kvasha <seregeykvasha@gmail.com>
2022-09-18 16:06:31 +0200

    add moving objects

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 let game = document.getElementById("game-board");
 let player = document.getElementById("player");
+let objects = document.getElementById("objects");
 let playerPosition = parseInt(
   window.getComputedStyle(player).getPropertyValue("top")
 );
+
 
 
 function movePlayerUp() {
@@ -19,13 +21,34 @@ function movePlayerDown() {
   }
 }
 
-function move(e) {
-  if (e.key == "ArrowUp") {
+function move(button) {
+  if (button.key == "ArrowUp") {
     movePlayerUp();
   }
-  if (e.key == "ArrowDown") {
+  if (button.key == "ArrowDown") {
     movePlayerDown();
   }
 }
+
+
+function createObjects(){
+    let objectLeft = 970;
+    let objectTop = Math.floor(Math.random() * 570);
+    let object = document.createElement('div');
+    object.setAttribute("class", "object");
+    objects.appendChild(object);
+    object.style.left = objectLeft + 'px';
+    object.style.top = objectTop + 'px';
+    function moveObjects(){
+        objectLeft -= 5;
+        object.style.left = objectLeft + 'px';
+        object.style.top = objectTop + 'px'
+    }
+    setInterval(moveObjects, 20);
+    setTimeout(createObjects, 2000);
+    
+
+}
+createObjects();
 
 document.addEventListener("keydown", move);
