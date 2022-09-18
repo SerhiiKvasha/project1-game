61f5e21a2da0fb9d6cafa6cf5f853061edd5e06e
Serhii Kvasha <seregeykvasha@gmail.com>
2022-09-18 14:47:48 +0200

    create functionality for player

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
-class Game {
-    startGame(){
-        console.log ("starting game");
-    }
+let game = document.getElementById("game-board");
+let player = document.getElementById("player");
+let playerPosition = parseInt(
+  window.getComputedStyle(player).getPropertyValue("top")
+);
+
+
+function movePlayerUp() {
+  if (playerPosition > 0) {
+    playerPosition -= 15;
+    player.style.top = playerPosition + "px";
+  }
 }
 
+function movePlayerDown() {
+  if (playerPosition < 500) {
+    playerPosition += 15;
+    player.style.top = playerPosition + "px";
+  }
+}
 
+function move(e) {
+  if (e.key == "ArrowUp") {
+    movePlayerUp();
+  }
+  if (e.key == "ArrowDown") {
+    movePlayerDown();
+  }
+}
 
-
-
-
-
-
-const newGame = new Game();
-newGame.startGame(); 
+document.addEventListener("keydown", move);
