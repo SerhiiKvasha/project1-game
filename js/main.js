2a437435ca03e202206718c5bd41330e162e6751
Serhii Kvasha <seregeykvasha@gmail.com>
2022-09-20 14:42:06 +0200

    add the start and gameover menu

--- js/main.js
+++ js/main.js
@@ -8,11 +8,9 @@ let playerTop = parseInt(
 );
 let playerLeft = parseInt(
   window.getComputedStyle(player).getPropertyValue("left")
-)
+);
 let score = 0;
-
-
-
+let lives = 3;
 function movePlayerUp() {
   if (playerTop > 0) {
     playerTop -= 15;
@@ -35,36 +33,57 @@ function move(button) {
     movePlayerDown();
   }
 }
-
-
-function createObjects(){
-    let objectLeft = 970;
-    let objectTop = Math.floor(Math.random() * 570);
-    let object = document.createElement('div');
-    object.setAttribute("class", "object");
-    objects.appendChild(object);
-    object.style.left = objectLeft + 'px';
-    object.style.top = objectTop + 'px';
-    function moveObjects(){
-      if(objectLeft < playerLeft + 30 && objectLeft > playerLeft && objectTop > playerTop -30 && objectTop < playerTop +100 ){
-        objects.removeChild(object);
-        clearInterval(interval);
-        score += 10;
-      }
-      if(objectLeft < playerLeft) {
-          console.log("game over,your score is" + score)
-          clearInterval(interval);
-          clearTimeout(timeout);
-        }
-        objectLeft -= 5;
-        object.style.left = objectLeft + 'px';
-        object.style.top = objectTop + 'px'
-    }
-    let interval = setInterval(moveObjects, 20);
-    let timeout = setTimeout(createObjects, 2000);
-    
-
-}
-createObjects();
-
 document.addEventListener("keydown", move);
+
+function createObjects() {
+  let objectLeft = 970;
+  let objectTop = Math.floor(Math.random() * 570);
+  let object = document.createElement("div");
+  object.setAttribute("class", "object");
+  objects.appendChild(object);
+  object.style.left = objectLeft + "px";
+  object.style.top = objectTop + "px";
+  function moveObjects() {
+    if (
+      objectLeft < playerLeft + 30 &&
+      objectLeft > playerLeft &&
+      objectTop > playerTop - 30 &&
+      objectTop < playerTop + 100
+    ) {
+      objects.removeChild(object);
+      clearInterval(interval);
+      clearTimeout(timeout);
+      score += 10;
+    }
+    if (objectLeft < playerLeft) {
+      clearInterval(interval);
+      clearTimeout(timeout);
+      objects.removeChild(object);
+      lives--;
+    }
+    if (lives === 0) {
+      gameOver();
+    }
+    objectLeft -= 5;
+    object.style.left = objectLeft + "px";
+    object.style.top = objectTop + "px";
+  }
+  let interval = setInterval(moveObjects, 20);
+  let timeout = setTimeout(createObjects, 2000);
+}
+
+function startGame() {
+  game.style.display = "block";
+  document.getElementById("start-game").style.display = "none";
+  document.getElementById("end-game").style.display = "none";
+  createObjects();
+}
+function gameOver() {
+  document.getElementById("start-game").style.display = "none";
+  game.style.display = "none";
+  document.getElementById("end-game").style.display = "block";
+}
+function tryAgain() {
+  location.reload();
+  return;
+}
